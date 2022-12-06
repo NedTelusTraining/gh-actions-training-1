68d025d903a5b6ce7b644f7a9b5cdd2059dda91b
Nery Herrera <nery.herrera@telusinternational.com>
2022-12-06 01:02:36 -0600

    fir issue for vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://nedtelustraining.github.io/',
+  base: 'https://nedtelustraining.github.io/gh-actions-training-1/',
   plugins: [react()]
 })
